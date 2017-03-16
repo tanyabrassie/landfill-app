4e8c78c72d6496b68de2b14b2e9488d5fedb0167
Tanya Brassie <tanyabrassie@TANYASCGMACHINE.home>
2017-03-16 16:45:06 -0400

    Updating filtering to get results from database rather than json file

--- routes/filter.js
+++ routes/filter.js
@@ -3,11 +3,14 @@ var router = express.Router();
 var fs = require('fs');
 var dataReader = require('../helpers/dataReader');
 var filterFunctions = require('../helpers/filterFunctions');
+var db = require('../helpers/db');
+
 
 
 
 router.post('/filter', function (req, res, next) {
 
+	//collect filter data
 	var filterData = {
 		yearStart: req.body.yearStart,
 		yearEnd: req.body.yearEnd,
@@ -15,21 +18,42 @@ router.post('/filter', function (req, res, next) {
 		state: req.body.state
 	}
 
+	//convert year data to Integer
 	var startRange = parseInt(filterData.yearStart);
 	var endRange = parseInt(filterData.yearEnd);
 
-	dataReader.getData(filterAndDisplay);
+	//get the collection	
+	db.get().collection('landfilldata', function (err, collection) {
+			
+			collection.find({'State': filterData.state, 'Current Landfill Status': filterData.status}).toArray(function (err, results){
+
+				results.forEach(function(item){
+
+					item['Year Landfill Opened'] = parseInt(item['Year Landfill Opened']);
+				});
+
+				var filteredResults = results.filter(function(landfill) {
+					return landfill['Year Landfill Opened'] >= startRange && landfill['Year Landfill Opened'] <= endRange;
+				});
+
+
+				console.log(filteredResults);
+				res.send(filteredResults);					
+			});	
+	});		
+
+	// dataReader.getData(filterAndDisplay);
 
 	
-	function filterAndDisplay(landfillArray) {
+	// function filterAndDisplay(landfillArray) {
 
-		var searchResults = filterFunctions.stateStatusFilter(landfillArray, filterData);
+	// 	var searchResults = filterFunctions.stateStatusFilter(landfillArray, filterData);
 
-		var filteredResults = filterFunctions.yearOpenedFilter(searchResults, filterData.yearStart, filterData.yearEnd);
+	// 	var filteredResults = filterFunctions.yearOpenedFilter(searchResults, filterData.yearStart, filterData.yearEnd);
 		
-		console.log(filteredResults);
-		res.send(filteredResults);
-	}	
+	// 	console.log(filteredResults);
+	// 	res.send(filteredResults);
+	// }	
 });
 
 module.exports = router;
